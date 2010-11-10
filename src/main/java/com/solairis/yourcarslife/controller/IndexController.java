4e593c8ceae2a96b2184d6f3514c8d5973e75cb6
Josh Paul Johnson <joshpauljohnson@gmail.com>
2010-11-10 07:46:14 -0500

    Added VehicleLogDao to the IndexController

--- src/main/java/com/solairis/yourcarslife/controller/IndexController.java
+++ src/main/java/com/solairis/yourcarslife/controller/IndexController.java
@@ -4,6 +4,8 @@
  */
 package com.solairis.yourcarslife.controller;
 
+import com.solairis.yourcarslife.data.dao.VehicleLogDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,6 +16,9 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class IndexController {
+
+	@Autowired
+	private VehicleLogDao VehicleLogDao;
 
 	@RequestMapping("/")
 	public ModelAndView index() {
