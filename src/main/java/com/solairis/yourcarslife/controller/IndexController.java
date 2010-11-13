5ac29fdf5896487d3a5e3cb4b81481ad341a8205
Josh Paul Johnson <joshpauljohnson@gmail.com>
2010-11-13 08:21:23 -0500

    Working basic security

--- src/main/java/com/solairis/yourcarslife/controller/IndexController.java
+++ src/main/java/com/solairis/yourcarslife/controller/IndexController.java
@@ -17,7 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class IndexController {
 
-	@Autowired
+	//@Autowired
 	private VehicleLogDao VehicleLogDao;
 
 	@RequestMapping("/")
