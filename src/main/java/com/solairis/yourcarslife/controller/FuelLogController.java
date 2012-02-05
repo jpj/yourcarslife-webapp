44c94d754ad6405617ed9c7af8a003c3ffe62ee6
Joshua P Johnson <joshpauljohnson@gmail.com>
2012-02-05 14:03:49 -0500

    Fixed imports

--- src/main/java/com/solairis/yourcarslife/controller/FuelLogController.java
+++ src/main/java/com/solairis/yourcarslife/controller/FuelLogController.java
@@ -5,10 +5,7 @@
 package com.solairis.yourcarslife.controller;
 
 import com.solairis.yourcarslife.command.FuelLogFormData;
-import com.solairis.yourcarslife.command.FuelLogFormData;
 import com.solairis.yourcarslife.data.domain.LogFuel;
-import com.solairis.yourcarslife.data.domain.User;
-import com.solairis.yourcarslife.data.domain.Vehicle;
 import com.solairis.yourcarslife.service.LogService;
 import com.solairis.yourcarslife.service.UserService;
 import com.solairis.yourcarslife.service.VehicleService;
@@ -16,17 +13,12 @@ import java.beans.PropertyEditor;
 import java.util.Date;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 /**
  *
