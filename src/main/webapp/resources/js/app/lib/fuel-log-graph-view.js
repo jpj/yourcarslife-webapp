c73ff5297fbcef985e4ac5f9cd90e5a76f8aa633
Josh Paul Johnson <joshpauljohnson@gmail.com>
2012-03-25 11:42:02 -0400

    Check fuel econ list before building graph. This fixes a bug where one could not enter a fuel log if they didn't already have at least one.

--- src/main/webapp/resources/js/app/lib/fuel-log-graph-view.js
+++ src/main/webapp/resources/js/app/lib/fuel-log-graph-view.js
@@ -30,41 +30,43 @@ solairis.ycl.view.FuelLogGraph = Backbone.View.extend({
 
 		this.$el.empty(); // Clear graph
 
-		$.jqplot(this.$el.attr("id"), [fuelEconomy], {
-//			//title: "My Graph",
-			grid: {
-				borderWidth: 1,
-				shadow: false
-			},
-			series:[
-				{
-					lineWidth: 1,
-					color: "#6EAB75",
+		if (fuelEconomy.length) {
+			$.jqplot(this.$el.attr("id"), [fuelEconomy], {
+				//title: "My Graph",
+				grid: {
+					borderWidth: 1,
 					shadow: false
-				}
-			],
-			axes:{
-				xaxis: {
-					renderer: $.jqplot.DateAxisRenderer,
-					rendererOptions: {tickRenderer:$.jqplot.CanvasAxisTickRenderer},
-					tickOptions: {
-						formatString: '%Y %b %#d'
+				},
+				series:[
+					{
+						lineWidth: 1,
+						color: "#6EAB75",
+						shadow: false
+					}
+				],
+				axes:{
+					xaxis: {
+						renderer: $.jqplot.DateAxisRenderer,
+						rendererOptions: {tickRenderer:$.jqplot.CanvasAxisTickRenderer},
+						tickOptions: {
+							formatString: '%Y %b %#d'
+						}
+					},
+					yaxis: {
+						tickOptions: {
+							formatString: '%.2f'
+						}
+						, min: 0
 					}
 				},
-				yaxis: {
-					tickOptions: {
-						formatString: '%.2f'
-					}
-					, min: 0
+				highlighter: {
+					sizeAdjust: 7.5
+				},
+				cursor: {
+					tooltipLocation: 'sw',
+					show: true
 				}
-			},
-			highlighter: {
-				sizeAdjust: 7.5
-			},
-			cursor: {
-				tooltipLocation: 'sw',
-				show: true
-			}
-		});
+			});
+		}
 	}
 });
