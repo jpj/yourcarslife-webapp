90b37152048805ee2f39c398f4d1e9af87da9dcc
Joshua P Johnson <joshpauljohnson@gmail.com>
2012-03-29 09:19:31 -0400

    Added user controller to return a user for now

---
+++ src/main/java/com/solairis/yourcarslife/controller/UserController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.solairis.yourcarslife.controller;
+
+import com.solairis.yourcarslife.data.domain.User;
+import com.solairis.yourcarslife.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author Joshua P Johnson
+ */
+@Controller
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+
+	@RequestMapping(value="/api/user/{userId}", method= RequestMethod.GET)
+	@Transactional
+	@ResponseBody
+	public User get(@PathVariable("userId") long userId) {
+		return this.userService.getUser(userId);
+	}
+
+}
