5d6ce5a0bfc5c0836c48ecdf24cacf0cdf7516ee
Joshua P Johnson <joshpauljohnson@gmail.com>
2013-02-17 10:05:17 -0500

    Fixed fuel log paging bug

--- src/main/webapp/resources/js/app/view/fuel-log-list-view.js
+++ src/main/webapp/resources/js/app/view/fuel-log-list-view.js
@@ -32,7 +32,7 @@ solairis.ycl.view.FuelLogList = Backbone.View.extend({
 		this.collection.fetch({
 			add: true,
 			data: {
-				offset: this.collection.length + 1,
+				offset: this.collection.length,
 				numResults: 10,
 				vehicleId: this.options.vehicleId
 			},
