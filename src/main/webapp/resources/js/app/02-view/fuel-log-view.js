67bebe06524bf13d1c67ba1143d575fdcdf6e27a
Josh Paul Johnson <joshpauljohnson@gmail.com>
2013-06-23 16:24:43 -0400

    Set initial focus, when opening a fuel log, to the date field.

--- src/main/webapp/resources/js/app/02-view/fuel-log-view.js
+++ src/main/webapp/resources/js/app/02-view/fuel-log-view.js
@@ -37,6 +37,7 @@ solairis.ycl.view.FuelLog = Backbone.View.extend({
 		this.$(".missedFillup input").get(0).checked = this.model.get("missedFillup");
 		this.$(".economy .number").text(economy);
 		this.$el.addClass("fuel-log-"+this.model.get("logId"));
+		this.$(".date input").focus();
 
 		return this;
 	},
